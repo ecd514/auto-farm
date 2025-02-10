fd460d5cc09097150fc77fbafb7ca11618e7bfe6
Aidan Kapuschinsky <kapuschino@cashnews.io>
2025-02-10 07:51:31 -0500

    Added basic restapi functionality that listens on localhost using flask

--- controller/contoller.py
+++ controller/contoller.py
@@ -1,8 +1,21 @@
-from weatherapi import weatherfuncs
+import threading
+# from weatherapi import weatherfuncs
+from websocket.flask_rest_api_app import start_api
+
+# def main():
+#    weatherfuncs.reqweather()
 
 
-def main():
-  weatherfuncs.reqweather()
+api_app = start_api()
+
+# def run_flask():
+#    """Runs the Flask app in a separate thread."""
+#    api_app.run(host='localhost', port=5000, debug=True, use_reloader=False)
+#
+# Start Flask in a background thread
+# flask_thread = threading.Thread(target=run_flask, daemon=True)
+# flask_thread.start()
+
 
 if __name__ == "__main__":
-  main()
+    api_app.run(host='localhost', port=5000, debug=True)
---
+++ controller/websocket/flask_rest_api_app.py
@@ -0,0 +1,23 @@
+# websocket/flask.py
+from flask import Flask, g
+from .db import init_db, get_db
+from .pump import pump_bp
+
+
+def start_api():
+    app = Flask(__name__)
+
+    # Register API blueprints.
+    app.register_blueprint(pump_bp)
+
+    # Ensure that the database connection is closed after each request.
+    @app.teardown_appcontext
+    def close_connection(exception):
+        db = getattr(g, '_database', None)
+        if db is not None:
+            db.close()
+
+    # Initialize the database.
+    init_db(app)
+
+    return app
